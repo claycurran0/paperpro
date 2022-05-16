cdde3de48fd885dad1cd8ae9da6ae99862fc9fb5
claycurran0 <ccurran@chicagobooth.edu>
2022-05-16 19:58:37 +0000

    created 3 tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  
+  root "portfolios#index"
+
+  devise_for :users
+  resources :posts
+  resources :portfolios
+  
+
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
