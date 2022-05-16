abcf74fc5049ab8f259ea8e8ee72d0dfb019932e
claycurran0 <ccurran@chicagobooth.edu>
2022-05-16 22:13:15 +0000

    added username edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :image_url])
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:username, :image_url])
+  end
+
 end
