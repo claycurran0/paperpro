cb08b243b0f55d0471db59261a81d7cfea2be10b
claycurran0 <ccurran@chicagobooth.edu>
2022-05-16 21:33:47 +0000

    completed sign in sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
 
-  
   root "portfolios#index"
 
   devise_for :users
   resources :posts
   resources :portfolios
   resources :assets
-  
-
-  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
