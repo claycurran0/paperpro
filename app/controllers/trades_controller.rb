43c1b206d52ffc80b5b32b781fc53317205d9adc
claycurran0 <ccurran@chicagobooth.edu>
2022-05-23 23:20:33 +0000

    added price pulls for holdings

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -5,16 +5,16 @@ class TradesController < ApplicationController
     data = query.quotes(ticker)
     price = data[ticker]['regularMarketPrice']
     quantity = params.fetch("input_quantity").to_f
-    portfolio = params.fetch("portfolio")
+    portfolio_id = params.fetch("portfolio_id")
 
     t = Trade.new
-    t.asset_id = Asset.find(ticker)
+    t.asset_id = Asset.find_by(ticker: ticker).id
     t.price = price
     t.quantity = quantity
     t.portfolio_id = portfolio_id
     t.save
 
-    redirect_back(fallback_location: root_path, notice: "Success! Price was #{price}, quantity was #{quantity}")
+    redirect_back(fallback_location: root_path, notice: "Success! Price was #{t.price}, quantity was #{t.quantity}, portfolio was #{t.portfolio_id}, asset was #{t.asset_id}")
   end
 
   def index
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,43 @@ class Portfolio < ApplicationRecord
   belongs_to :user
   has_many :trades
 
+  def cash_balance
+    balance = 1000000
+    self.trades.each do |t|
+      cashflow = t.quantity * t.price
+      balance = balance - cashflow
+    end
+
+    return balance
+  end
+
+  def quotes(tickers)
+    query = BasicYahooFinance::Query.new
+    quotes = query.quotes(tickers)
+    return quotes
+  end
+
+  def holdings
+    holdings = []
+    tickers = []
+
+    Asset.all.each do |a|
+      quantity = Trade.where({ :portfolio_id => self.id, :asset_id => a.id}).sum(:quantity)
+      if quantity > 0
+        output = { :asset => a, :quantity => quantity }
+        holdings.push(output)
+        tickers.push(a.ticker) 
+      end
+    end
+
+    quotes = quotes(tickers)
+
+    holdings.each do |h|
+      ticker = h[:asset].ticker
+      price = quotes[ticker]['regularMarketPrice']
+      h[:current_price] = price
+    end
+
+    return holdings
+  end
 end
