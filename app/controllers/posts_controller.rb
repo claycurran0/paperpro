4f8121ead0529d9df6e076c875bd3af435d17333
claycurran0 <ccurran@chicagobooth.edu>
2022-05-29 02:47:26 +0000

    adding card wrapers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,7 @@ class PostsController < ApplicationController
   # GET /posts/new
   def new
     @post = Post.new
+    @post.portfolio_id = params.fetch(:portfolio)
   end
 
   # GET /posts/1/edit
@@ -65,6 +66,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :body, :portfolio, :image_url)
+      params.require(:post).permit(:title, :subtitle, :portfolio_id, :body, :image_url)
     end
 end
